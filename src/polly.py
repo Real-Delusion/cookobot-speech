62737237a419c19dce3a92b22cee2d754393a286
almiab1 <almiab1@epsg.upv.es>
2020-06-13 18:58:09 +0200

    documented polly class

--- src/polly.py
+++ src/polly.py
@@ -3,10 +3,16 @@ import pygame
 import time
 import os
 
-
 class Polly:
 
     def __init__(self, awsSession, rutaFicheroPistas):
+        """This is the contructor of Polly class
+
+        Args:
+            awsSession (array): Credential of Amazon Web Services
+            rutaFicheroPistas (string): Path where we save the files
+        """                  
+        
         self.awsSession = awsSession
         # Propiedades configurables del servicio
         # Ruta donde se guardaran las pistas
@@ -29,11 +35,24 @@ class Polly:
     
     
     def configurarVoz(self, vozId, outputFormat):
+        """This is a method of Polly class
+
+        Args:
+            vozId (string): The name of the voice
+            outputFormat (string): Format of the audio fila
+        """        
         self.vozId = vozId
         self.outputFormat = outputFormat
         
     # Metodo que genera una pista de audio a partir de un texto
     def generarAudio(self, texto, nombreArchivo):
+        """This is a method of Polly class
+
+        Args:
+            texto (string): Text to reproduce
+            nombreArchivo (string): The name of file tha we can generate
+        """        
+        
         print("Generate sound file")
         response = self.polly_client.synthesize_speech(
                 VoiceId=self.vozId,
@@ -52,6 +71,11 @@ class Polly:
     
     # Metodo que reproduce la pista
     def reproducirAudio(self,nombreArchivo):
+        """This is a method of Polly class
+
+        Args:
+            nombreArchivo (string): Name of the file that we can play
+        """        
         archivo = self.rutaFicheroPistas+"/"+nombreArchivo # Ruta donde se encuentra el archivo
         
         #  Uso de la libreria pygame para la reporduccion de audio
@@ -64,9 +88,16 @@ class Polly:
     
     # Metodo que reproduce la pista
     def borrarAudio(self,nombreArchivo):
+        """This is a method of Polly class
+
+        Args:
+            nombreArchivo (string): Name of file that we can delete
+        """        
+        
         print("Delete sound file")
         archivo = self.rutaFicheroPistas+"/"+nombreArchivo # Ruta donde se encuentra el archivo
         os.remove(archivo) # Borrar archivo
+
 
 # Exmaple
 # polly = Polly(AWSsesion, './../audios')
